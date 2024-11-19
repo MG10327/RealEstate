d0c285c7c94a92ed44eff8b8c50fa995cc3302a6
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-18 23:25:21 -0500

    add filter functinoality

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {Flex, Select, Box, Text, Input, Spinner, Icon, Button} from "@chakra-ui/react"
-import { useRouter } from 'next/router'
+import { useRouter} from 'next/router'
 import {mdCancel} from "react-icons/md"
 import Image from 'next/image'
 
@@ -9,8 +9,25 @@ import { filterData, getFilterValues } from '../utils/filterData'
 const SearchFilters = () => {
 
   const [filters, setFilters] = useState(filterData) // This sets the default array to be the filters array mapped above
+  const router = useRouter()
 
 
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues)
+
+    values.forEach((item) => {
+      if(item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value
+      }
+    })
+
+    router.push({ pathname: path, query: query });
+  };
+
+  
   return (
     <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
       {filters.map((filter) =>
