2ee27fc5c290c92c670874185bc46803d38d0ac4
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-18 23:55:48 -0500

    image scrollbar setup

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Image from "next/image"
+import {Box, Icon, Flex} from "@chakra-ui/react"
+import {ScrollMenu, scrollMenu, VisibilityContext} from "react-horizontal-scrolling-menu"
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
 
-const ImageScrollbar = () => {
+const LeftArrow = () => {
+  const { scrollPrev} = useContext(VisibilityContext)
+
   return (
-    <div>ImageScrollbar</div>
+    <Flex justifyContent="center" alignItems="center" marginRight="1">
+      <Icon as={FaArrowAltCircleLeft} onClick={scrollPrev} fontSize="2xl" cursor="pointer" />
+    </Flex>
+  )
+
+}
+
+
+
+const ImageScrollbar = ({data}) => {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{overflow:'hidden'}}>
+      {data.map((item) =>
+        <Box width="910px" itemId={item.id} overflow="hidden" p="1" >
+          <Image
+
+            alt='property'
+            placeholder='blur'
+            blurDataURL={item.url}
+            src={item.url}
+            width={1000}
+            height={500}
+            sizes='(max-width: 500px) 100px, (max-width:1023px) 400px, 1000px'
+          />
+        </Box>
+      )}
+    </ScrollMenu>
   )
 }
 
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {Box, Flex, Spacer, Text, Avatar} from "@chakra-ui/react"
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import millify from 'millify';
+import ImageScrollbar from "../../components/ImageScrollbar"
 
-const [id] = () => {
-  return (
-    <div>[id]</div>
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+
+const PropertyDetails = ({PropertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, type, purpose, furnishingStatus, amenities, photos}}) => (
+    <Box maxWidth="1000px" margin="auto" p="4">
+      {photos && <ImageScrollbar data={photos} />}
+    </Box>
   )
-}
 
-export default [id]
+export default PropertyDetails
+
+export async function getServerSideProps({params: {id}}) {
+  const data = await fetchApi(`${baseUrl}/properties/details/list?locationExternalIDs=${id}`)
+
+  return {
+    props: {
+      PropertyDetails: data
+    }
+  }
+}
