2d3ad7ddaae21500b475e093f17c7a99c8d29fd7
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-19 17:19:14 -0500

    add comment to address

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,12 +4,11 @@ import axios from "axios"
 export const baseUrl = "https://bayut.p.rapidapi.com"
 
 export const fetchApi = async (url) => {
-
   const { data} = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-	    'x-rapidapi-key': process.env.RAPID_API_KEY || ""
-    }
+	    'x-rapidapi-key': 'd847eeabebmsh8dd2ceb29fd1b78p1350ffjsnde203ac66ef2' // This is FAR from ideal but I am working out some issues with vercel to use environment variables in production instead.
+    } 
   })
 
   return data
