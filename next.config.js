ae37a36c0a216b4e709bd624059d06bea118e7ee
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-19 16:40:22 -0500

    more vercel debugging

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images:{
     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
-},
+  },
+  env: {
+    RAPID_API_KEY: process.env.RAPID_API_KEY,
+  }
 }
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,12 +4,11 @@ import axios from "axios"
 export const baseUrl = "https://bayut.p.rapidapi.com"
 
 export const fetchApi = async (url) => {
-  console.log("Environment Variable Key:", process.env.NEXT_PUBLIC_RAPID_API_KEY);
 
   const { data} = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-	    'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY || ""
+	    'x-rapidapi-key': process.env.RAPID_API_KEY || ""
     }
   })
 
