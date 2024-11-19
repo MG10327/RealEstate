99b925c730833b6c015277d654cbee98aee32b1d
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-18 23:16:31 -0500

    render filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Flex, Select, Box, Text, Input, Spinner, Icon, Button} from "@chakra-ui/react"
 import { useRouter } from 'next/router'
 import {mdCancel} from "react-icons/md"
 import Image from 'next/image'
 
+import { filterData, getFilterValues } from '../utils/filterData'
+
 const SearchFilters = () => {
 
+  const [filters, setFilters] = useState(filterData) // This sets the default array to be the filters array mapped above
 
 
   return (
     <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
-
+      {filters.map((filter) =>
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+            >
+              {filter?.items?.map((item) =>
+                <option value={item.value} key={item.value}>{item.name}</option>
+              )}
+            </Select>
+        </Box>
+      )}
     </Flex>
   )
 }
