7d7b2cd60fa75eaa03b53b020f22dc2265b87b68
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-19 16:34:10 -0500

    logging to test vercel issues

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,11 +4,13 @@ import axios from "axios"
 export const baseUrl = "https://bayut.p.rapidapi.com"
 
 export const fetchApi = async (url) => {
+  console.log("Environment Variable Key:", process.env.NEXT_PUBLIC_RAPID_API_KEY);
+
   const { data} = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
 	    'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY || ""
-    } 
+    }
   })
 
   return data
