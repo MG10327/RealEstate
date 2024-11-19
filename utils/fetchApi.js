d33268e8576bece2d8e6071ccaea6740ee6df92d
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-19 00:34:27 -0500

    empty commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -8,7 +8,7 @@ export const fetchApi = async (url) => {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
 	    'x-rapidapi-key': 'd847eeabebmsh8dd2ceb29fd1b78p1350ffjsnde203ac66ef2'
-    }
+    } 
   })
 
   return data
